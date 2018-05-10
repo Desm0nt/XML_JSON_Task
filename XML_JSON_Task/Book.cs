52b3579707cba22750f48384026153990415bd8f
Desm0nt <logran2@gmail.com>
2018-05-10 15:28:42 +0300

    Добавьте файлы проекта.

---
+++ XML_JSON_Task/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_JSON_Task
+{
+    class Book
+    {
+        public string Bookname { get; set; }
+        public string AFirstName { get; set; }
+        public string ALastName { get; set; }
+        public int PageCount { get; set; }
+        public int PublishYear { get; set; }
+    }
+}
---
+++ XML_JSON_Task/Books.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_JSON_Task
+{
+    class Books
+    {
+        List<Book> Book;
+
+        Books()
+        {
+            Book = new List<Book>();
+        }
+
+        public void AddBook(string bookname, string fname, string lname, int pcount, int pyear)
+        {
+            Book.Add(new Book { Bookname = bookname, AFirstName = fname, ALastName = lname, PageCount = pcount, PublishYear = pcount });
+        }
+
+        public void SaveToXML(string path)
+        {
+
+        }
+
+        public void ReadFromXML(string path)
+        {
+
+        }
+
+        public void SaveToJSON(string path)
+        {
+
+        }
+
+        public void ReadFromJSON(string path)
+        {
+
+        }
+    }
+}
